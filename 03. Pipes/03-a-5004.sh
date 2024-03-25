0a5b97d92d2d4c3d32b24ad5fd0fff5213016231
DobromirPeev <102480232+DobromirNPeev@users.noreply.github.com>
2024-03-25 10:44:59 +0200

    Update 03-a-5004.sh

--- 03. Pipes/03-a-5004.sh
+++ 03. Pipes/03-a-5004.sh
@@ -1,5 +1,5 @@
 cat /etc/passwd | cut -d ':' -f 5| egrep ' \<[а-яА-Я]{,7}\>' | xargs -I{} grep {} /etc/passwd
 
-cat /etc/passwd | cut -d ':' -f 5| cut -d ',' -f 1 | grep -E ' [а-яА-я]{7,}$' > 5003.out
+cat /etc/passwd | cut -d ':' -f 5| cut -d ',' -f 1 | grep -E ' [а-яА-я]{,7}$' > 5003.out
 cat /etc/passwd | grep -f 5003.out
 
