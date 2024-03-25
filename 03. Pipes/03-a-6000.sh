f7777390644e4ea42aeb5aa050f6913ad170b7a3
DobromirPeev <102480232+DobromirNPeev@users.noreply.github.com>
2024-03-25 10:53:37 +0200

    Update 03-a-6000.sh

--- 03. Pipes/03-a-6000.sh
+++ 03. Pipes/03-a-6000.sh
@@ -2,4 +2,5 @@ cat ~/emp.data | awk ' END { print NR}'
 cat ~/emp.data | awk ' NR == 3  { print $0}'
 cat ~/emp.data | awk ' { print $NF}'
 cat ~/emp.data | awk 'END { print $NF}'
+cat ~/emp.data | awk 'NF > 4 { print $0}'
 
