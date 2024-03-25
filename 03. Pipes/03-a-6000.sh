a0f5fea80002166dc62345f2837d86ffdb32b97e
DobromirPeev <102480232+DobromirNPeev@users.noreply.github.com>
2024-03-25 11:04:34 +0200

    Update 03-a-6000.sh

--- 03. Pipes/03-a-6000.sh
+++ 03. Pipes/03-a-6000.sh
@@ -5,4 +5,5 @@ cat ~/emp.data | awk 'END { print $NF}'
 cat ~/emp.data | awk 'NF > 4 { print $0}'
 cat ~/emp.data | awk '$NF > 4 { print $0}'
 cat ~/emp.data | awk 'BEGIN {sum=0} { sum +=NF } END {print sum}'
+cat ~/emp.data | awk 'BEGIN {max=0; row=0} $NF > max { max=$NF; row=NR} END { print max,row}'
 
